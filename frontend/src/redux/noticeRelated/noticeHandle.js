e1ddee76efa3961b4e0c982b256e64934676a58f
Gopal Kumar Singh <gopalkumarsingh7007@gmail.com>
2026-01-19 00:05:07 +0530

    ...

--- frontend/src/redux/noticeRelated/noticeHandle.js
+++ frontend/src/redux/noticeRelated/noticeHandle.js
@@ -6,11 +6,13 @@ import {
     getError
 } from './noticeSlice';
 
+const BASE_URL = process.env.REACT_APP_BASE_URL.replace(/\/$/, "");
+
 export const getAllNotices = (id, address) => async (dispatch) => {
     dispatch(getRequest());
 
     try {
-        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}List/${id}`);
+        const result = await axios.get(`${BASE_URL}/${address}List/${id}`);
         if (result.data.message) {
             dispatch(getFailed(result.data.message));
         } else {
